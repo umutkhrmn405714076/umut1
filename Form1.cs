cb7f9689c929d2a562384f6f634eabf84618282d
umutkhrmn405714076 <101084367+umutkhrmn405714076@users.noreply.github.com>
2022-03-07 10:29:59 +0300

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,63 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace Arraylist_Uygulama_3
+namespace personel_maaş_takibi
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
-            InitializeComponent();
+            
         }
-        ArrayList sehirler = new ArrayList();
 
-        private void btnEkle_Click(object sender, EventArgs e)
+        private void btnHesapla_Click(object sender, EventArgs e)
         {
-            sehirler.Add(txtSehirler.Text);
-            Listele();
+
         }
-
-         public void Listele()
-        {
-            Şehirler.Items.Clear();
-            foreach (string sehir in sehirler)
-            {
-                Şehirler.Items.Add(sehir);
-            }
-        }
-
-         private void btnAra_Click(object sender, EventArgs e)
-         {
-             if (sehirler.Contains(txtSehirler.Text))
-             {
-                 label2.Text = "Aranan Değer Bulundu.";
-                 Şehirler.SelectedIndex = sehirler.IndexOf(txtSehirler.Text);
-             }
-             else
-             {
-                 label2.Text = "Aranan Değer Bulunamadı.";
-             }
-         }
-
-         private void btnSil_Click(object sender, EventArgs e)
-         {
-             int indexNo = Şehirler.SelectedIndex;
-             sehirler.RemoveAt(indexNo);
-             Listele();
-         }
-
-         private void btnGüncelle_Click(object sender, EventArgs e)
-         {
-             int indexNo = Şehirler.SelectedIndex;
-             sehirler[indexNo] = txtSehirler.Text;
-             Listele();
-         }
-
-         private void btnArayaEkle_Click(object sender, EventArgs e)
-         {
-             int indexNo = Şehirler.SelectedIndex;
-             sehirler.Insert(indexNo,txtSehirler.Text);
-             Listele();
-         }
     }
 }
